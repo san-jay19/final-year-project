e14d71f8e92a17b59a780bc3dcf7d44166208ef2
san-jay19 <sanjay1942004@gmail.com>
2024-02-17 22:55:12 +0530

    separated navbar

--- project/aboutUs.php
+++ project/aboutUs.php
@@ -6,60 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About Us</title>
     <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 20px;
-        }
-
-        h1 {
-            color: #333;
-        }
-
-        p {
-            margin-bottom: 10px;
-        }
-
-        header {
-            background-color: #333;
-            color: #fff;
-            padding: 10px;
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            border-bottom: 2px solid #fff;
-        }
-
-        .logo img {
-            width: 50px;
-            height: 50px;
-            border-radius: 5px;
-        }
-
-        nav ul {
-            list-style: none;
-            display: flex;
-        }
-
-        nav ul li {
-            margin-right: 20px;
-        }
-
-        nav a {
-            text-decoration: none;
-            color: #fff;
-            font-weight: bold;
-            font-size: 16px;
-        }
-
-        body,
-        h1,
-        h2,
-        h3,
-        p,
-        ul {
-            margin: 0;
-            padding: 0;
-        }
+       
 
         body {
             font-family: 'Arial', sans-serif;
@@ -111,20 +58,7 @@
 </head>
 
 <body>
-    <header>
-        <div class="logo">
-            <img src="logo.png" alt="JobWalla!">
-        </div>
-        <nav>
-            <ul>
-                <li><a href="index.php">Home</a></li>
-                <li><a href="jobSeekers.php">Job Seekers</a></li>
-                <li><a href="employers.php">Employers</a></li>
-                <li><a href="aboutUs.php">About Us</a></li>
-                <li><a href="contact.php">Contact</a></li>
-            </ul>
-        </nav>
-    </header>
+<?php include('navBar.php') ?>
     <br>
     <br>
 
